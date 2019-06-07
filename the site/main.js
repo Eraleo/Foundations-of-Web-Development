09a5a9866b913b06cf7a201dda0fd7f521d4cb65
Era <era.leo@gmail.com>
2019-06-07 13:18:34 +0200

    d

---
+++ the site/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    
+    setTimeout(function(){
+        $('.loadingPage').fadeOut(1000, function() {
+            $('.page1').fadeIn(1000);
+        });
+        
+    }, 1000);
+
+    
+    
+    //$(".loadingPageBtn").click(function(event){
+        //event.preventDefault();
+        //$('.loadingPage').hide();
+        //$('.page1').show();
+        //console.log("Bhenchod!");
+    //});
+
+});
+//$(document).ready(function() {
+    //console.log('ready!');
+    
+//    $('.myclass').hide();
+//
+//    $('.myclass').show();
+//})
+
+//$('.myclass').hover(function(){
+    // mouse over
+    
+//    $(this).css('color','blue');
+//}, function() {
+    // mouse over
+//});
+
+//let textPos = $('.mytext').offset();
+
+//$('.floatinBox').css('left', textPos.left).css('top', textPos.top);
+
+
+//$('.muyclass').fadeIn(600);
